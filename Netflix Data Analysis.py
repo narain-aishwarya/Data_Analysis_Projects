bc0d97bf56a74bda2ea7af64b26ed96bcee3dad1
Aishwarya Narain <134004148+narain-aishwarya@users.noreply.github.com>
2024-01-07 18:07:34 +0530

    Update Netflix Data Analysis.py

--- Netflix Data Analysis.py
+++ Netflix Data Analysis.py
@@ -1,1 +1,8 @@
 #This data analysis on based on Netfix data of 2019. <Currently working on 2024 >
+import numpy as np
+import pandas as pd
+import plotly.express as px #for data visualization
+from textblob import TextBlob # for sentiment analysis.
+
+data = pd.read_csv("/kaggle/input/netflix-shows/netflix_titles.csv")
+data.shape
