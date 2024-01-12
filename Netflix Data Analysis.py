e7589ea235e70fd6678cdc98c6eb7e63e16be91d
Aishwarya Narain <134004148+narain-aishwarya@users.noreply.github.com>
2024-01-12 23:50:31 +0530

    Update Netflix Data Analysis.py

--- Netflix Data Analysis.py
+++ Netflix Data Analysis.py
@@ -6,3 +6,10 @@ from textblob import TextBlob # for sentiment analysis.
 
 data = pd.read_csv("/kaggle/input/netflix-shows/netflix_titles.csv")
 data.shape
+data.columns
+#Now , looking at the Distribution of Content before starting the analysis.
+d = data.groupby(['rating']).size().reset_index(name='counts')
+pieChart = px.pie(d,values='counts',names='rating',
+                 title="Distribution of Content Rating on Netflix",
+                 color_discrete_sequence = px.colors.qualitative.Set1)
+pieChart.show()
