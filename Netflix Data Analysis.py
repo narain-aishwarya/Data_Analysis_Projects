c479fbe6b454849c179f5ab1518c3509a758e4a5
Aishwarya Narain <134004148+narain-aishwarya@users.noreply.github.com>
2024-01-23 22:51:52 +0530

    Update Netflix Data Analysis.py

--- Netflix Data Analysis.py
+++ Netflix Data Analysis.py
@@ -13,3 +13,17 @@ pieChart = px.pie(d,values='counts',names='rating',
                  title="Distribution of Content Rating on Netflix",
                  color_discrete_sequence = px.colors.qualitative.Set1)
 pieChart.show()
+
+#Now analysis the top director.
+data['director']=data['director'].fillna('No director specified')
+filtered_directors = pd.DataFrame()
+filtered_directors = data['director'].str.split(',',expand=True).stack()
+filtered_directors = filtered_directors.to_frame()
+filtered_directors.cloumns = ['Director']
+directors = filtered_directors.groupby(['Director']).size().reset_index(name='Total Content')
+directors = directors[directors.Director != 'No Director Specified']
+directors = directors.sort_values(by=['Total Content'],asending = False)
+directorsTop5 = directors.head()
+directorsTop5 = directorsTop5.sort_values(by=['Total Content'])
+fig1 = px.bar(directorsTop5, x='Total Content', y='Director', title='Top 5 directors on Netflix')
+fig1.show()
