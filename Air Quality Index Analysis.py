43aee1e1b140d7bc711c18aba87f1c46be48a596
Aishwarya Narain <134004148+narain-aishwarya@users.noreply.github.com>
2024-04-02 23:06:58 +0530

    Update Air Quality Index Analysis.py

--- Air Quality Index Analysis.py
+++ Air Quality Index Analysis.py
@@ -6,3 +6,6 @@ import plotly.graph_objects as go
 pio.templates.default = "plotly_white"
 data = pd.read_csv("/kaggle/input/new-delhi-air-quality/NewDelhi_Air_quality.csv")
 print(data.head())
+
+print(data.describe())
+
